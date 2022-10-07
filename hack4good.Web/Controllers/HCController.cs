8ad920a5055ab0bfae89de8fca0de92b9fdf5b54
bird_egop <e_sinitsa@head-point.ru>
2022-10-08 01:44:30 +0300

    add healthcheck

---
+++ hack4good.Web/Controllers/HCController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hack4good.Web.Controllers;
+
+public class HCController : HackController
+{
+    [HttpGet]
+    public async Task<IActionResult> Check()
+    {
+        return Ok(1);
+    }
+}
